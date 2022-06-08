513765c728a130b36ae52da67f3a195ecd84105b
Viktar <1105714@gmail.com>
2022-06-08 11:09:44 +0400

    init

---
+++ JS/sum_args.js
@@ -0,0 +1,8 @@
+/**
+ Write the function that takes any number of arguments
+ of type number and return the sum of all the arguments
+ */
+function sum(){
+}
+console.log(sum(1,2,3)) // expected 6
+console.log(sum(1,2,3,4)) // expected 10
